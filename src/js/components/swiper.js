ce40d0132725732b495eb91ad24e111921b9f4ae
Albert Akhmetov <albert.akhmetov344@gmail.com>
2024-09-30 17:01:56 +0500

    Created partners slider

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
@@ -18,6 +19,56 @@ export const useInsightSlider = () => {
     breakpoints: {
       993: {
         centeredSlides: false,
+      },
+    },
+  });
+};
+
+export const usePartnersSlider = () => {
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener('change', checker);
+    checker();
+  };
+
+  resizableSwiper('(max-width: 992px)', '.partners__slider', {
+    modules: [Autoplay],
+    speed: 3000,
+    loop: true,
+    slidesPerView: 2.8,
+    spaceBetween: 100,
+    autoplay: {
+      delay: 0,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      380: {
+        slidesPerView: 3,
+      },
+
+      576: {
+        slidesPerView: 4,
+      },
+
+      768: {
+        slidesPerView: 4,
       },
     },
   });
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ import '../assets/scss/main.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useInsightSlider } from './components/swiper.js';
+import { usePartnersSlider } from './components/swiper.js';
 
 useTheme();
 useBurger();
 useInsightSlider();
+usePartnersSlider();
