bfe7c6c95bf1e9116fe14de58270e2913a9adfc5
Albert Akhmetov <albert.akhmetov344@gmail.com>
2024-10-11 00:08:42 +0500

    created contact:contact section and added contact:partners section

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import '../assets/scss/contact-hero.scss';
+
+//components
+import { useTheme } from './components/home/theme.js';
+import { useBurger } from './components/home/burger.js';
+import { usePartnersSlider } from './components/home/partners-slider.js';
+
+useTheme();
+useBurger();
+usePartnersSlider();
