2e0f6580aafe3d1f7003b4614ff9afc109ad59dc
Albert Akhmetov <albert.akhmetov344@gmail.com>
2024-10-12 00:37:51 +0500

    Created contact:get-started section and custom validation

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,8 +4,12 @@ import '../assets/scss/contact-hero.scss';
 //components
 import { useTheme } from './components/home/theme.js';
 import { useBurger } from './components/home/burger.js';
+import { useAccordionFooter } from './components/footer.js';
+import { useTestInputMask } from '../js/components/contact/phone.js';
 import { usePartnersSlider } from './components/home/partners-slider.js';
 
 useTheme();
 useBurger();
+useAccordionFooter();
+useTestInputMask();
 usePartnersSlider();
